a7ae4540014ffff888af4a3c8287cb20bb60b127
Gábor Domonkos <gabor.domonkos@lombiq.com>
2019-09-04 00:23:35 +0200

    Adding support to deserialize the categories data of the groups

---
+++ Elvanto.Net/Models/GroupCategories.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Elvanto.Net.Models
+{
+    public class GroupCategories
+    {
+        [JsonProperty("category")]
+        public List<Category> Category { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
